de9ac181d283777ecd72a3599aa0744521d47a38
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-06-10 15:33:55 +0530

    added disable for testing

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App(){
       <Modal
         open={isModalOpen}
         onClose={() => setIsModalOpen(false)}
+        disableBackdropClick={true}
        sx={{
         position: 'absolute',
         left: '30%',
